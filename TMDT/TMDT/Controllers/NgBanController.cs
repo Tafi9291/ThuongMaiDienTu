cd093ac4aeeb0141a27d82c70989d2b289b18b35
Tafi <havinhtai712@gmail.com>
2023-10-04 10:38:49 +0700

    First commit

---
+++ TMDT/TMDT/Controllers/NgBanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TMDT.Controllers
+{
+    public class NgBanController : Controller
+    {
+        // GET: NgBan
+        public ActionResult KenhNgBan()
+        {
+            return View();
+        }
+    }
+}
---
+++ TMDT/TMDT/Controllers/NgMuaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TMDT.Controllers
+{
+    public class NgMuaController : Controller
+    {
+        // GET: NgMua
+        public ActionResult TrangChu()
+        {
+            return View();
+        }
+    }
+}
