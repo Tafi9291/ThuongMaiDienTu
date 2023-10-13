6d9081491bd7cd955036e489a4e5a47d3188f66f
Tafi <havinhtai712@gmail.com>
2023-10-13 23:16:45 +0700

    Xu ly san pham Admin (front-end)

---
+++ TMDT/TMDT/Areas/Admin/Controllers/QLSanPhamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TMDT.Areas.Admin.Controllers
+{
+    public class QLSanPhamController : Controller
+    {
+        // GET: Admin/QLSanPham
+        public ActionResult QLSanPham()
+        {
+            return View();
+        }
+    }
+}
