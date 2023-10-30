31462a1417d27c46d6d4c868277546c359d9188c
Sunaru <thanhtuantran1006@gmail.com>
2023-10-30 17:35:17 +0700

    Quản lý người dùng

--- TMDT/TMDT/Controllers/NgMuaController.cs
+++ TMDT/TMDT/Controllers/NgMuaController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using TMDT.Models;
 namespace TMDT.Controllers
 {
     public class NgMuaController : Controller
     {
+        TMDTEntities db=new TMDTEntities();
         // GET: NgMua
         public ActionResult TrangChu()
         {
+
             return View();
         }
 
