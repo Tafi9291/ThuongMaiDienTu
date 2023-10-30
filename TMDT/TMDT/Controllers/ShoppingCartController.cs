700679e0d2d7844d60468ef88226e4eb7c780500
Tafi <havinhtai712@gmail.com>
2023-10-30 16:03:45 +0700

    giao diện giỏ hàng

---
+++ TMDT/TMDT/Controllers/ShoppingCartController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TMDT.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        // GET: ShoppingCart
+        public ActionResult ShowToCart()
+        {
+            return View();
+        }
+
+        // GET: ChuaCoHang
+        public ActionResult ChuaCoHang()
+        {
+            return View();
+        }
+
+        public ActionResult Complete()
+        {
+            return View();
+        }
+
+        public ActionResult CheckOut()
+        {
+            return View();
+        }
+    }
+}
